b5b284eac97c7c3bd5b2970b2c32b03a292225cc
bobby <bobby@DESKTOP-6V2GR6M>
2023-09-13 22:42:00 -0600

    Generic text input was applied to the edit part of the details

--- list-detail/src/components/movieEdit.tsx
+++ list-detail/src/components/movieEdit.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import TextInput from "./textInput";
 import {Movie} from "../models/movie";
 
 interface MovieEditProps {
@@ -40,59 +41,35 @@ export const MovieEdit: React.FC<MovieEditProps> = ({
   return (
     <form className="container needs-validation" >
       <div className="row">
-        <label className="form-label" htmlFor="title">Title:* </label>
-        <div className="input-group has-validation">
-          <input
-            className={`form-control ${isTitleValid ? "is-valid": "is-invalid"}`}
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Add movie title"
-            onChange={(e) => setEditedTitle(e.currentTarget.value)}
-            value={editedTitle}
-            aria-describedby="titlevalidation"
-            required
-          />
-          <div id="titlevalidation" className="valid-feedback">Great Movie</div>
-          <div id="titlevalidation" className="invalid-feedback">Needs to be greater than 3 characters</div>
-        </div>
+        <TextInput
+          label= "Title:*"
+          value={editedTitle}
+          onChange={setEditedTitle}
+          placeholderText="Add movie title"
+          validationRules={(value) => value.length >= 2}
+          feedbackMessage="Must be at least 2 characters."
+        />
       </div>
       <div className="row">
-        <label className="form-label" htmlFor="runtime">Runtime:*</label>
-        <div className="input-group has-validation">
-          <input
-            type="text"
-            className={`form-control ${isRunTimeValid ? "is-valid": "is-invalid"}`}
-            required
-            placeholder="Add length of movie"
-            id="runtime"
-            name="runtime"
-            aria-describedby="runtimevalidation"
-            onChange={(e) => setEditedRunTime(e.currentTarget.value)}
-            value={editedRunTime}
-            />
-          <div id="runtimevalidation"className="valid-feedback">Great Time</div>
-          <div id="runtimevalidation"className="invalid-feedback">Needs to be greater than 0</div>
-        </div>
+        <TextInput
+          label= "RunTime:*"
+          value={editedRunTime}
+          onChange={setEditedRunTime}
+          placeholderText="Add length of movie"
+          validationRules={(value) => value.length >= 2}
+          feedbackMessage="Must be at least 2 characters."
+        />
       </div>
       <div className="row">
         <div className="col col-md-6">
-          <label className="form-label" htmlFor="year">Year:*</label>
-          <div className="input-group has-validation">
-            <input
-              required
-              type="text"
-              name="year"
-              id="year"
-              aria-describedby="yearvalidation"
-              className={`form-control ${isYearValid ? "is-valid": "is-invalid"}`}
-              placeholder="Add the year of the movie"
-              onChange={(e) => setEditedYear(e.currentTarget.value)}
-              value={editedYear}
-            />
-            <div id="yearvalidation" className="valid-feedback">Great Year</div>
-            <div id="yearvalidation" className="invalid-feedback">Must put a year</div>
-          </div>
+          <TextInput
+            label= "Year:*"
+            value={editedYear}
+            onChange={setEditedYear}
+            placeholderText="Add movie year"
+            validationRules={(value) => value.length <= 4}
+            feedbackMessage="Must be no more than 4 characters."
+          />
         </div>
         <div className="col col-md-6">
           <label className="form-label">Rating:*</label>
@@ -112,27 +89,19 @@ export const MovieEdit: React.FC<MovieEditProps> = ({
               <option>4</option>
               <option>5</option>
             </select>
-            <div id="ratingvalidation" className="valid-feedback">Great rating</div>
             <div id="ratingvalidation" className="invalid-feedback">Need a rating</div>
           </div>
         </div>
       </div>
       <div className="row">
-        <label className="form-label">Description:* </label>
-        <div className="input-group has-validation">
-          <input
-            type="text"
-            id="description"
-            aria-describedby="descriptionvalidation"
-            className={`form-control ${isDescValid ? "is-valid": "is-invalid"}`}
-            placeholder="Add movie description"
-            onChange={(e) => setEditedDesc(e.currentTarget.value)}
-            value={editedDesc}
-            required
-          />
-          <div id="descriptionvalidation" className="valid-feedback">Great Year</div>
-            <div id="descriptionvalidation" className="invalid-feedback">Missing input</div>
-        </div>
+        <TextInput
+          label= "Description:*"
+          value={editedDesc}
+          onChange={setEditedDesc}
+          placeholderText="Add movie description"
+          validationRules={(value) => value.length >= 2}
+          feedbackMessage="Must be at least 2 characters."
+        />
       </div>
       <br></br>
       <button className="btn btn-primary" type="submit" onClick={handleSave}>Save</button>
